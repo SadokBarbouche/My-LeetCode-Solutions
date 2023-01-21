c1bdbf3b254ae2acc7f948803aeba128c77e3671
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2023-01-21 19:37:45 +0100

    Time: 583 ms (23.28%), Space: 15.5 MB (20.40%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)
+        i = 0
+        if nums[0] == target :
+            return 0
+        for i in range(start,end):
+            middle = (end - start )//2
+            if nums[i] == target:
+                return i
+            elif nums[i] < nums[middle]:
+                end = middle
+            elif nums[i] > nums[middle]:
+                start = middle
+        return -1
