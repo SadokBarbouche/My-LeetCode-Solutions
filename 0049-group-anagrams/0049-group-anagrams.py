56ea20d1b8a1fdc133dbb36f6b5acb63989714c9
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2023-04-22 11:49:19 +0100

    Time: 108 ms (43.50%), Space: 17.4 MB (70.10%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        s = ["".join(sorted(s)) for s in strs]
+        z = zip(strs,s)
+        d = {i:[] for i in set(s)}
+        for i in z:
+            d[i[1]].append(i[0])
+        ans = []
+        for i in d.values():
+            ans.append(i)
+        return ans
