cba3902004516088a111fd3c2417fbacab70a8c1
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2023-08-05 23:49:18 +0100

    Time: 77 ms (5.26%), Space: 16.3 MB (75.49%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        n = len(students)
+        ans = 0
+        for _ in range(100*n):
+            if len(students) == 0:
+                break
+            if students[0] != sandwiches[0]:
+                students.append(students.pop(0))
+            else:
+                students.pop(0)
+                sandwiches.pop(0)
+                ans += 1
+        return n - ans
