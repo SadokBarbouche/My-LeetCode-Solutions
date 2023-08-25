99378b419d2dc0d8d2fe509d2dc584e739db953a
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2023-08-25 18:54:10 +0100

    Time: 289 ms (79.07%), Space: 24.2 MB (35.55%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        def dfs(grid, sr, sc, visited):
+            nRows = len(grid)
+            nCols = len(grid[0])
+            
+            if sr < 0 or sc < 0 or sr >= nRows or sc >= nCols or grid[sr][sc] == '0' or (sr, sc) in visited:
+                return
+            
+            visited.add((sr, sc))
+            
+            dfs(grid, sr + 1, sc, visited)
+            dfs(grid, sr - 1, sc, visited)            
+            dfs(grid, sr, sc + 1, visited)
+            dfs(grid, sr, sc - 1, visited)
+            
+            # Do not remove (sr, sc) from visited here
+            
+        nIslands = 0
+        visited = set()
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1' and (r, c) not in visited:  # Add a check to avoid redundant DFS calls
+                    dfs(grid, r, c, visited)
+                    nIslands += 1
+                    
+        return nIslands
