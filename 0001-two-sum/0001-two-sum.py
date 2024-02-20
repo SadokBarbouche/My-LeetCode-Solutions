646617505c15fe5b58168e91bf0750572d73a83a
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2024-02-20 12:50:56 +0100

    Time: 63 ms (57.28%), Space: 17.8 MB (69.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,23 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        freq = {i:[] for i in nums}
-        for i in range(len(nums)):
-            freq[nums[i]].append(i)
-        for i in range(len(nums)):
-            if target-nums[i] in freq:
-                ind_1 = freq[nums[i]][0]
-                ind_2 = freq[target-nums[i]][0]
-                if ind_1 != ind_2:
-                    return [ind_1,ind_2]
-                else:
-                    if len(freq[nums[i]])>1:
-                        ind_1 = freq[nums[i]][0]
-                        ind_2 = freq[nums[i]][1]
-                        return [ind_1,ind_2]
-                    elif len(freq[target-nums[i]])>1:
-                        ind_1 = freq[target-nums[i]][0]
-                        ind_2 = freq[target-nums[i]][1]
-                        return [ind_1,ind_2]
-                        
+        num_indices = {}
+        # Checked the last year implementation and how shitty my code was        
+        for i, num in enumerate(nums):
+            if target - num in num_indices:
+                return [num_indices[target - num], i]
+            num_indices[num] = i
+        
         return []
-                
