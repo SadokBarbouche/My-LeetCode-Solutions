77b6bd49142778619031d136eb7f0a205a92c5a7
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2024-03-03 23:09:58 +0100

    Time: 55 ms (54.62%), Space: 16.8 MB (39.22%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        max_duration = releaseTimes[0]
+        max_duration_keys = [keysPressed[0]]
+        for i in range(1, len(releaseTimes)):
+            duration = releaseTimes[i] - releaseTimes[i - 1]
+            if duration > max_duration:
+                max_duration = duration
+                max_duration_keys = [keysPressed[i]]
+            elif duration == max_duration:
+                max_duration_keys.append(keysPressed[i])
+        return max(max_duration_keys)
