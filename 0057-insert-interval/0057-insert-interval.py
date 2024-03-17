c5be1e9ac89986d9c368e25a377ff24ff2552eae
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2024-03-17 13:05:39 +0100

    Time: 73 ms (71.36%), Space: 20.1 MB (9.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,7 +1,9 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         extended_intervals = [(start, end) for start, end in intervals] + [(newInterval[0], newInterval[1])]
+        # print(extended_intervals)
         extended_intervals.sort(key=lambda x: x[0])
+        # print(extended_intervals)
         merged_intervals = []
         for start, end in extended_intervals:
             if not merged_intervals or merged_intervals[-1][1] < start:
