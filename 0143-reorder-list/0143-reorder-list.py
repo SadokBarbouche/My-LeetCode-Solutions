c2d6892f652e79381e86b1b891e1cf456cd8a23a
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2024-03-23 01:08:50 +0100

    Time: 55 ms (61.35%), Space: 24.5 MB (87.25%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
+        
+        temp = head
+        elements = []
+        while temp:
+            elements.append(temp.val) 
+            temp = temp.next
+        n = len(elements)
+        i, j = 0, n - 1
+        temp_head = head
+        while i < j:
+            temp_head.val = elements[i]
+            temp_head = temp_head.next
+            temp_head.val = elements[j]
+            temp_head = temp_head.next
+            i += 1
+            j -= 1
+        if i == j:
+            temp_head.val = elements[i]
