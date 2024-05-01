bd7c0384bdbd68eeddae550b48280730e3811324
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2024-05-01 15:16:11 +0100

    Time: 34 ms (63.06%), Space: 16.7 MB (9.30%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -2,10 +2,8 @@ class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         if not ch in word:
             return word
-        rev = ""
         for i in range(len(word)):
             if word[i] == ch:
-                rev = word[:i+1][::-1]
-                return rev + word[i+1:]
+                return word[:i+1][::-1] + word[i+1:]
         return ""
             
